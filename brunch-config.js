a6016594f914bac2ea8dea05dfbfdf66db8b6913
Jvo A. Maurer <jvo.maurer@gmail.com>
2024-06-29 19:16:55 +0200

    no message

--- brunch-config.js
+++ brunch-config.js
@@ -1,37 +1,37 @@
 module.exports = {
-    server: {
-        port: 3002
-    },
-    autoReload: {enabled: true},
-    files: {
-        javascripts: {
-            joinTo: {
-                'vendor.js': /^(?!app|vendor)/,
-                'app.js': /^app/
-            }
-        },
-    },
-    conventions: {
-        ignored: [
-            /^vendor/,
-            /^etc/,
-            /^node_modules/,
-        ],
-    },
-    paths: {
-        public: 'public'
-    },
-    plugins: {
-        babel: {
-            presets: [
-                ["@babel/preset-env"]
-            ],
-            // plugins: ["syntax-async-functions", "transform-regenerator"]
-        },
-        browserSync: {
-            enabled: true,
-            port: 3333,
-            logLevel: "debug"
-        }
-    }
+	server: {
+		port: 3002,
+	},
+	autoReload: {enabled: true},
+	files: {
+		javascripts: {
+			joinTo: {
+				'vendor.js': /^(?!app|vendor)/,
+				'app.js': /^app/,
+			},
+		},
+	},
+	conventions: {
+		ignored: [
+			/^vendor/,
+			/^etc/,
+			/^node_modules/,
+		],
+	},
+	paths: {
+		public: 'public',
+	},
+	plugins: {
+		babel: {
+			presets: [
+				["@babel/preset-env"],
+			],
+			// plugins: ["syntax-async-functions", "transform-regenerator"]
+		},
+		browserSync: {
+			enabled: true,
+			port: 3333,
+			logLevel: "debug",
+		},
+	},
 }
